48c00f652d971e45d6e2e451546c50afd55f4030
eironmoisesdupra-coder <eironmoisesdupra@gmail.com>
2026-09-26 07:12:23 +0800

    Bump cache for loading ground and lighting update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='the-hollow-v24';
+const CACHE='the-hollow-v25';
 const ASSETS=['./index.html','./manifest.webmanifest','./icon.svg','./assets/images/main-menu-bg.jpg','./assets/images/prologue-hallway-bg.png','./assets/images/loading-photo-noah-elias.png','./assets/images/loading-photo-elias-hollow.png','./vendor/three.min.js'];
 
 self.addEventListener('install',event=>{
