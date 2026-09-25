708d8f4530a5faec89cc270ece9a0e086213b6fb
eironmoisesdupra-coder <eironmoisesdupra@gmail.com>
2026-09-25 19:24:24 +0800

    Bump cache for responsive menu fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='the-hollow-v6';
+const CACHE='the-hollow-v7';
 const ASSETS=['./index.html','./manifest.webmanifest','./icon.svg','./assets/images/main-menu-bg.jpg'];
 
 self.addEventListener('install',event=>{
