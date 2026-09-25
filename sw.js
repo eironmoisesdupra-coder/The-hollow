e1681a15962a3444f806141151ad818ab0b92305
eironmoisesdupra-coder <eironmoisesdupra@gmail.com>
2026-09-25 20:23:28 +0800

    Bump cache for static transition update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='the-hollow-v12';
+const CACHE='the-hollow-v13';
 const ASSETS=['./index.html','./manifest.webmanifest','./icon.svg','./assets/images/main-menu-bg.jpg','./assets/images/loading-hollow-bg.png'];
 
 self.addEventListener('install',event=>{
